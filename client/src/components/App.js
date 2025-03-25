ac228e3133a6458bab6b247b73d05469321bcb2f
cullen kaberere <kabesh86@gmail.com>
2025-03-25 13:17:43 +0300

    checkout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,6 +4,7 @@ import HomePage from "./HomePage";
 import RegisterClient from "./RegisterClient";
 import Login from "./Login"; // ✅ Corrected import
 import CarDetails from "./CarDetails";
+import Checkout from "./Checkout";
 import ServiceManagement from "./ServiceManagement";
 import Navbar from "./NavBar"; // Import Navbar
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/register-client" element={<RegisterClient />} />
         <Route path="/login" element={<Login />} /> {/* ✅ Corrected component name */}
         <Route path="/car-details" element={<CarDetails />} />
+        <Route path="/checkout" element={<Checkout />} />
         <Route path="/service-management" element={<ServiceManagement />} />
       </Routes>
     </Router>
