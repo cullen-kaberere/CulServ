03565778b3976bf193d658d9786be4b51948ec6a
cullen kaberere <kabesh86@gmail.com>
2025-07-04 19:48:11 +0300

    update database URI in configuration

--- server/app.py
+++ server/app.py
@@ -19,7 +19,7 @@ bcrypt = Bcrypt(app)
 
 # Configuration
 app.config.update(
-    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI', 'postgresql://culserv_9mkn_user:xRVXOpksM7SKWhV5n2F4c9wngxEufwUq@dpg-d1jvkrvdiees73ck063g-a/culserv_9mkn'),
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI', 'postgresql://culserv_9mkn_user:xRVXOpksM7SKWhV5n2F4c9wngxEufwUq@dpg-d1jvkrvdiees73ck063g-a.oregon-postgres.render.com/culserv_9mkn'),
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
     JSON_COMPACT=False,
     SECRET_KEY=os.environ.get('SECRET_KEY') or 'dev-key-change-me',
